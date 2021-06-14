aee8e5f70d89edabf7aa29c44a419d316273e5d6
Chanoga <chanb001@vodacom.co.tz>
2021-06-14 17:05:50 +0300

    added delete,clear and insert_af functions

--- Singly_Linked_list.c
+++ Singly_Linked_list.c
@@ -9,7 +9,6 @@ List* head = NULL;
         ->Inser node at the end of the list
         ->Insert node before a given node
         ->Insert node after the given node
-        ->Delete first node
         ->Delete any node
         ->Sort list in Ascending order
         ->Sort list in Descending order
@@ -17,6 +16,7 @@ List* head = NULL;
         ->find the biggest node in the list
         ->Find the smallest node in the list
         ->Find node in the list
+        ->clear list/delete entire list
 */
 //display list nodes
 void display(List* list){
@@ -103,21 +103,101 @@ List* insert_bef(List *list,int val,unsigned int pos){
         prevP ->next  = element;
         element->next = nextP;
         prevP = element;
-           //debuging reasons only
-        printf(("\
-        prevP[%p][%d]  \
-        prevP->next[%p] \
-        element[%p][%d]  \
-        element->next[%p] \
-        nextP[%p][%d] \
-        nextP->next[%p] \n\
-        "),prevP,
-        prevP->node,
-        prevP->next,
-        element,element->node
-        ,element->next,nextP,nextP->node,nextP->next);
+        //    //debuging reasons only
+        // printf(("\
+        // prevP[%p][%d]  \
+        // prevP->next[%p] \
+        // element[%p][%d]  \
+        // element->next[%p] \
+        // nextP[%p][%d] \
+        // nextP->next[%p] \n\
+        // "),prevP,
+        // prevP->node,
+        // prevP->next,
+        // element,element->node
+        // ,element->next,nextP,nextP->node,nextP->next);
     }
     return list;
+}
+
+//insert node after a given node
+List* insert_aft(List* list,int val,unsigned int pos){
+    List *element,*prevP,*nextP;
+
+    element = (List*)malloc(sizeof(List));
+    element ->node = val;
+
+    if(list == NULL)
+    {
+        list = element;
+        element->next = NULL;
+    }
+    else
+    {
+        prevP = list;
+        nextP = prevP ->next;
+
+        while(prevP != NULL && prevP ->node != pos){
+            prevP = nextP;
+            nextP = nextP->next;
+        }
+        prevP ->next = element;
+        element ->next = nextP;
+        prevP = element;
+    }
+    return list;
+}
+
+//delete an entire list
+List *clear(List* list){
+    List *tmp;
+    if(list != NULL)
+    {
+        while(list != NULL) list = list->next;
+
+        tmp = list;
+        free(tmp);//remove node from the heap
+    }
+    else puts("The list is alread Empty: ");
+    return list;
+}
+
+//delete a single given node from the list (bug on deleting first or last node)
+List* delete_node(List* list,int val){
+    List *ptr,*temp,*prevP;
+    if(list == NULL){
+        puts("Empty list,nothing to delete");
+        return NULL;
+    }
+    else{
+        prevP = list;
+        ptr = list->next;
+
+        //if the node is the first node in the list
+        if(prevP->node == val){
+            temp = prevP;
+            prevP = prevP->next;
+            free(temp);
+        }
+        else{
+            while(ptr->node != val && ptr->next != NULL) 
+            {
+                prevP = ptr;
+                ptr = ptr->next;
+            }
+            //is our node the last node ..?
+            if(ptr->next == NULL){
+                temp = ptr;
+                prevP ->next = NULL; //prevP now point to the last node
+                free(temp);
+            }
+            temp = ptr;
+            ptr = ptr->next;
+            prevP ->next = ptr;
+            free(temp);
+        }
+        return list;
+    }
 }
  int main (void){
      unsigned short opt;
@@ -130,6 +210,9 @@ List* insert_bef(List *list,int val,unsigned int pos){
         1: Insert Beginning \n\
         2: Insert End \n\
         3: Insert Before a given node\n\
+        4: Insert After a given node\n\
+        5: Clear \n\
+        6: Delete node\n\
         "),stdout);
         scanf("%hu",&opt);
 
@@ -158,6 +241,24 @@ List* insert_bef(List *list,int val,unsigned int pos){
             scanf("%d",&val);
             head = insert_bef(head,val,pos);
             break;
+
+            case 4:
+            puts("Enter Insertion point(node to be inserted after)");
+            scanf("%u",&pos);
+            puts("Enter value");
+            scanf("%d",&val);
+            head = insert_aft(head,val,pos);
+            break;
+
+            case 5:
+            head = clear(head);
+            break;
+
+            case 6:
+            puts("Enter node to delete");
+            scanf("%d",&val);
+            head = delete_node(head,val);
+            break;
         }
-    }while(opt >=0 && opt <4);
+    }while(opt >=0 && opt <7);
 }
