69224d237e679f7af8cdc096ecb5c0b806f71ac8
chanoga <chanogab@gmail.com>
2024-04-30 14:10:28 +0300

    adding functionality

--- Trees/binary_search_tree.c
+++ Trees/binary_search_tree.c
@@ -1,4 +1,15 @@
 /**
+ * 				BINARY SEARCH TREE/ORDERED BINARY TREE
+ * 				-------------------------------------
+ *
+ * 			def:
+ * 				Is a variant of binary trees in which nodes are arranged in orders.
+ *
+ * 			In binary search tree, all nodes in the left subtree are less than a root node.
+ * 			Correspondingly all nodes in the right subtree are either equal or greater than
+ * 			root node. The same rule is applied to every subtree in the tree.
+ *
+ *
  * Implementation of the Binary Search Tree:
  * 
  * Operations:
@@ -10,7 +21,7 @@
  * -->Mirror Tree
  * -->Find the largest Node
  * -->Find the smallest Node
- * -->Find if a given node is in the tree
+ * -->Search node in the tree
  * -->Delete an entire tree
  * -->Delete leaf node
  * -->Delete a node with only one child
@@ -18,114 +29,215 @@
  */
 
 #include<stdio.h>
-#include<malloc.h>
 #include<stdlib.h>
-#include<string.h>
+#include<malloc.h>
 
-typedef struct BinarySearchTree{
-    
-    struct Node
-    {
-        int age;
-        char fname[30];
-        char lname[30];
-    }node;
+struct btree{
+	int node;
+	struct btree *left,*right;
+};
 
-    struct BinarySearchTree *left,*right;
+struct btree* root = NULL;/* tree is initially empty */
 
-}Tree;
+/* pre-order traversing*/
+void pre_order(struct btree *tree){
 
-Tree *root = NULL;
+	if(tree == NULL) return;
 
-void pre_order(Tree *tree)
-{
-    if(tree == NULL) return;
-
-    else{
-        printf(("\t \
-        %s \t |\
-        %s \t |\
-        %d \t |\n\
-        "),tree->node.fname,tree->node.lname,tree->node.age);
-        pre_order(tree->left);
-        pre_order(tree->right);
-    }
+	else if(tree != NULL){
+		fprintf(stdout,"%d -> ",tree -> node);
+		pre_order(tree -> left);
+		pre_order(tree -> right);
+	}
 }
 
-Tree *insert(Tree *tree,char* fname,char* lname,int age)
-{
-    Tree* elem,*ptr,*node_ptr;
+/*in-order */
+void in_order(struct btree* tree){
 
-    elem = (Tree*)malloc(sizeof(Tree));
-    strcpy(elem->node.fname,fname);
-    strcpy(elem->node.lname,lname);
-    elem->node.age = age;
-    elem->left = NULL;
-    elem->right = NULL;
+	if(tree != NULL){
+		in_order(tree -> left);
+		fprintf(stdout,"%d ->",tree -> node);
+		in_order(tree -> right);
+	}
+}
 
-    if(tree == NULL)
-    {
-        tree = elem;
-        tree->left = NULL;
-        tree->right = NULL;
-    }
 
-    else
-    {
-        node_ptr = NULL;
-        ptr = tree;
+/*post-order */
+void post_order(struct btree* tree){
 
-        while(ptr != NULL)
-        {
-            node_ptr = ptr;
+	if(tree != NULL){
+		post_order(tree -> left);
+		post_order(tree -> right);
+		fprintf(stdout,"%d ->",tree -> node);
+	}
+}
+/*insert node*/
+struct btree *ins_node(struct btree *tree,int val){
+	struct btree *ptr,*nptr,*prvptr;
 
-            ptr = (ptr->node.age > age)? ptr->left:ptr->right;
-        }
-        if(node_ptr->node.age > age) node_ptr->left = elem;
+	ptr = (struct btree*)malloc(sizeof(struct btree));
 
-        else node_ptr->right = elem;
-    }
+	ptr -> node = val;
 
-    return tree;
+	/*if tree is empty, our node is the root node*/
+	if(tree == NULL){
+		tree = ptr;
+		tree -> left = tree->right = NULL;
+	}
+
+	/*tree is not empty*/
+	else{
+		nptr = tree;
+
+		while(nptr != NULL){
+			prvptr = nptr;
+
+			nptr = (nptr -> node > val)? nptr -> left : nptr -> right;
+
+		}
+
+		if(prvptr -> node > val) prvptr -> left = ptr;
+
+		else if( prvptr -> node <= val) prvptr -> right = ptr;
+
+	}
+
+	return tree;
+}
+
+/*find smallest node*/
+struct btree *sm_node(struct btree *tree){
+
+	if((tree == NULL) || (tree -> left == NULL)) return tree;
+
+	else return sm_node(tree -> left);
+}
+
+/*find largest node */
+struct btree* ls_node(struct btree *tree){
+
+	if((tree == NULL) || tree -> right == NULL) return tree;
+
+	else return ls_node(tree -> right);
+
+}
+
+/*search node */
+int  s_node(struct btree *tree,int val){
+
+	struct btree* ptr,*nptr;
+
+	if(tree == NULL) return -1;
+
+	else{
+		ptr = tree;
+		nptr = ((ptr -> node > val) && (ptr -> node != val))? sm_node(ptr) : ls_node(ptr);
+
+		/*value equal root node*/
+		if(ptr -> node == val)
+			return ptr -> node;
+
+		if(nptr -> node == val)
+			return nptr -> node;
+
+		else{
+			return -1;
+		}
+	}
+}
+
+/*mirror tree*/
+struct btree* mirror(struct btree *tree){
+	struct btree *ptr;
+
+	if(tree!=NULL){
+		mirror(tree ->left);
+		mirror(tree ->right);
+		ptr=tree ->left;
+		ptr ->left = ptr ->right;
+		tree ->right = ptr;
+
+		return tree;
+	}
+}
+
+/*delete entire tree*/
+struct btree* del_tree(struct btree *tree){
+	if(tree != NULL){
+		del_tree(tree -> left);
+		del_tree(tree -> right);
+		free(tree);
+	}
+
+	return NULL;
 }
 int main(void)
 {
     unsigned short opt;
-    struct Node *n;
-    char fname[30],lname[30];
-    int age;
+    int val;
+    char *instructions ="1: pre-order \n"
+	    		"2: in-order \n"
+			"3: post-order \n"
+			"4: insert node \n"
+			"5: smallest node\n"
+			"6: Largest node\n"
+			"7: Search node\n"
+			"8: Mirror \n"
+			"9: Delete tree\n"
+			"0: quit\n";
+
     do
     {
-        puts(("\n \
-        1: Pre-Order Traverse \n\
-        2: In-Order Traverse \n\
-        3: Post-Order Traverse \n\
-        4: Insert Node \n\
-        "));
-
+	printf("\n%s\n",instructions);
         scanf("%hu",&opt);
 
         switch(opt)
         {
+		case 0:
+			break;
             case 1:
              pre_order(root);
             break;
 
             case 2:
+	    in_order(root);
+	    break;
             case 3:
+	    post_order(root);
             break;
 
             case 4:
-            puts("Enter first name");
-            scanf("%s",fname);
-            puts("Enter last name");
-            scanf("%s",lname);
-            puts("Enter age");
-            scanf("%d",&age);
-            root = insert(root,fname,lname,age);
+            puts("Enter Value");
+            scanf("%d",&val);
+            root = ins_node(root,val);
             break;
+
+	    case 5:
+	    printf("Smallest Node: %d\n",sm_node(root)->node);
+	    break;
+
+	    case 6:
+	    printf("Largest Node: %d\n",ls_node(root) -> node);
+	    break;
+
+	    case 7:
+	    puts("Enter search value");
+	    scanf("%d",&val);
+	    val = s_node(root,val);
+	    if(val == -1) printf("Not Found: %d\n",val);
+
+	    else printf("Found: %d\n",val);
+	    break;
+
+	    case 8:
+	    root = mirror(root);
+	    break;
+
+	    case 9:
+	    root = del_tree(root);
+	    break;
         }
-    } while (opt > 0 && opt < 5);
+    } while (opt > 0 && opt < 10);
     
     return 0;
 }
